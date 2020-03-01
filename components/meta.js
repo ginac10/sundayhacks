643c19ef6f60677cbdee77301c14a2854207bc5b
hue choi <amazinghue.choi@gmail.com>
2020-02-29 17:37:09 -0800

    .

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
 export default ({
-  title = 'AngelHacks – Dedicated to giving back!',
+  title = 'SundaylHacks – Dedicated to giving back!',
   description = 'A 12-hour hackathon for middle and high schoolers in Los Angeles.',
-  image = 'https://angelhacks.org/static/banner-inverted.png',
-  url = 'https://angelhacks.org'
+  image = 'https://sundayhacks.org/static/images/banner-inverted.png',
+  url = 'https://sundayhacks.org'
 }) => (
   <Head>
     <title>{title}</title>
@@ -12,34 +12,28 @@ export default ({
     <meta name="twitter:title" content={title} />
     <meta name="og:url" content={url} />
     <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="AngelHacks" />
-    <meta name="twitter:site" content="@angelhacks_la" />
+    <meta property="og:site_name" content="SundayHackcs" />
     <meta name="description" content={description} />
     <meta property="og:description" content={description} />
-    <meta name="twitter:description" content={description} />
     <meta property="og:image" content={image} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={image} />
     <script
       type="application/ld+json"
       dangerouslySetInnerHTML={{
         __html: JSON.stringify({
           '@context': 'https://schema.org',
           '@type': 'Organization',
-          name: 'angelhacks',
-          url: 'https://angelhacks.org',
-          logo: 'https://angelhacks.org/logo_inverted.png',
+          name: 'sundayhacks',
+          url: 'https://sundayhacks.org',
+          logo: 'https://sundayhacks.org/static/images/logo_inverted.png',
           sameAs: [
-            'https://twitter.com/angelhacks_la',
-            'https://www.instagram.com/angelhacks.la',
-            'https://www.facebook.com/angelhacks_la'
+            'https://sundayhacks.org'
           ],
           contactPoint: [
             {
               '@type': 'ContactPoint',
-              email: 'hello@angelhacks.org',
+              email: 'hello@sundayhacks.org',
               contactType: 'customer support',
-              url: 'https://angelhacks.org'
+              url: 'https://sundayhacks.org'
             }
           ]
         })
@@ -51,14 +45,14 @@ export default ({
         __html: JSON.stringify({
           '@context': 'http://schema.org/',
           '@type': 'Event',
-          name: 'AngelHacks',
+          name: 'SundayHacks',
           startDate: '2019-10-26T9:00',
           endDate: '2019-10-26T21:00',
           description:
-            'Register today for 12 hours of coding, fun, free food, and prizes. AngelHacks will be on October 26, 2019 and open to all middle and high school students.',
+            'Register today for 12 hours of coding, fun, free food, and prizes. SundayHacks will be on October 26, 2019 and open to all middle and high school students.',
           isAccessibleForFree: true,
-          url: 'https://angelhacks.org',
-          image: 'https://angelhacks.org/static/logo-inverted.png',
+          url: 'https://sundayhacks.org',
+          image: 'https://sundayhacks.org/static/images/logo-inverted.png',
           location: {
             '@type': 'Place',
             name: 'Snapchat Headquarters',
@@ -78,7 +72,7 @@ export default ({
           offers: {
             '@type': 'Offer',
             name: 'Free Admission',
-            url: 'https://angelhacks.com',
+            url: 'https://sundayhacks.org',
             price: '0',
             priceCurrency: 'USD',
             availability: 'https://schema.org/InStock',
@@ -87,7 +81,7 @@ export default ({
           },
           performer: {
             '@type': 'PerformingGroup',
-            name: 'AngelHacks Ad Astra Team'
+            name: 'SundayHacks Ad Astra Team'
           }
         })
       }}
