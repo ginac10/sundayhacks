5af2b84aa9e6edede1a3134f23c26a535bb20378
Hyungsun Choi <amazinghue.choi@gmail.com>
2020-03-19 23:47:00 -0700

    link

--- components/avatar.js
+++ components/avatar.js
@@ -3,7 +3,7 @@ import { Image, Link as A } from 'rebass'
 
 export default ({ size = 50, light = false, ...props }) => (
   <Link href="https://sundayhacks.org">
-    <A sx={{ cursor: 'pointer', lineHeight: 0 }} href="/">
+    <A sx={{ cursor: 'pointer', lineHeight: 0 }} href="https://sundayhacks.org/">
       <Image
         {...props}
         src="/static/profiles/logonotext.png"
